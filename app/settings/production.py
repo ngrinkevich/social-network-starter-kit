5f1b8a9617e8d4126f4350dd19352416eb44c7b4
Nik G <subzeromedia@gmail.com>
2015-02-24 15:05:36 +0000

    prod settings demo

--- app/settings/production.py
+++ app/settings/production.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
-# SECRET_KEY = ')0*14wp#g8si#31c6&$)^(3n_$dc!jipnh79ql1tus!%(b9&o-'
+SECRET_KEY = ')0*14wp#g8si#31c6&$)^(3n_$dc!jipnh79ql1tus!%(b9&o-'
 
-# DEBUG = False
-# TEMPLATE_DEBUG = DEBUG
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
 
-# DATABASES = {
-#     'default': {
-#         'NAME': '',
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1'
-#     },
+DATABASES = {
+    'default': {
+        'NAME': 'demo_db',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'USER': 'demo_user',
+        'PASSWORD': 'demo_pass',
+        'HOST': '127.0.0.1'
+    },
    
-# }
+}
---
+++ app/wsgi_prod.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for subzeromedia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os, site
+
+site.addsitedir('/home/rushhourcrush_env/lib/python2.7/site-packages/')
+os.environ["DJANGO_SETTINGS_MODULE"] = "app.settings.production"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
